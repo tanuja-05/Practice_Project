5345b88a173a7e6165120c744a6713d9e245018e
tanuja-05 <tanujamaurya99@gmail.com>
2026-01-06 22:30:30 +0530

    updated

--- server/controllers/libraryController.js
+++ server/controllers/libraryController.js
@@ -1,6 +1,7 @@
 let {libraryModel}= require('../models/librayModel.model')
 
 let bookInsert = (req,res)=>{
+
     let {bCode,bName,bAuthor,bPrice} = req.body;
     let insertObj = new libraryModel({
         bCode,
@@ -20,4 +21,36 @@ let bookInsert = (req,res)=>{
      })
 }
 
-module.exports={bookInsert};
+let bookView = async(req,res)=>{
+    let viewObj= await libraryModel.find();
+    res.send({
+        status:1,
+        msg:"Available Books",
+        viewObj
+    })
+}
+
+let bookDelete = async(req,res)=>{
+        let bookId=req.params.id;
+        let delRes = await libraryModel.deleteOne({_id:bookId});
+        res.send({
+            status:1,
+            msg:"Book Deleted",
+            delRes
+        })
+}
+
+let bookUpdate = async(req,res)=>{
+    let bookId = req.params.id;
+    let {bCode,bName,bAuthor,bPrice}=req.body;
+    let newObj={bCode,bName,bAuthor,bPrice};
+    let updateRes= await libraryModel.updateOne({_id:bookId},newObj);
+    res.send({
+        status:1,
+        msg:"Data updated",
+        updateRes
+    })
+}
+
+
+module.exports={bookInsert,bookView,bookDelete,bookUpdate};
--- server/routes/libraryRouter.js
+++ server/routes/libraryRouter.js
@@ -1,7 +1,11 @@
 let express = require('express');
-const { bookInsert } = require('../controllers/libraryController');
+const { bookInsert, bookView, bookDelete, bookUpdate } = require('../controllers/libraryController');
 let libraryRouter = express.Router();
 
 libraryRouter.post('/insert',bookInsert);
+libraryRouter.get('/view',bookView);
+libraryRouter.delete('/delete/:id',bookDelete)
+libraryRouter.put('/update/:id',bookUpdate)
+
 
 module.exports=libraryRouter;
